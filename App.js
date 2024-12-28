2e4d313698213fe98cd3e0ee605597d959524217
Abdullah Darwish <engabdullahtarekdarwish@gmail.com>
2024-12-28 17:40:47 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,17 @@
 const refr = document.getElementById('abdullah');
-refr.onclick = ()=>{
+const Nav = document.getElementById('nav');
+const Btn = document.getElementById('menu-button');
+
+Btn.addEventListener("click", () => {
+    if (window.innerWidth <= 573) {
+    if (Nav.style.display === 'block') {
+        Nav.style.display = 'none';
+    } else {
+        Nav.style.display = 'block';
+    }
+}
+  });
+  
+  refr.onclick = ()=>{
     location.reload();
 }
